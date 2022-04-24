d6db786c15dcc4326f770fe90e397fef57bdc044
Naveen <naveengali80@gmail.com>
2022-04-24 12:24:46 +0530

    Test2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Deployment through Ci Cd Pipieline</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
